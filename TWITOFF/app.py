e6f0688d09c045a792e77cd7af537c21cdf1b1a5
johanaluna <johanaluna@users.noreply.github.com>
2019-10-16 14:07:53 -0400

    updated after lecture

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -4,14 +4,16 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 def create_app():
     """create and configures an instance of a flask app"""
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    app.config['ENV'] = config('ENV')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #app.config['ENV'] = config('ENV') #should change this later to production
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
     DB.init_app(app)
 
     @app.route('/')
@@ -19,19 +21,23 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
+    @app.route('/user', methods=['POST','GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None,message=''):
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets=[]
+        return render_template('user.html', title=name, tweets=tweets,
+        message=message)
+
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title='DB reset', users=[])
-
-    # @app.route('/user/<username>')
-    # def profile(username):
-    #     return '{}\'s profile'.format(escape(username))
-    #
-    # with app.test_request_context():
-    #     print(url_for('index'))
-    #     print(url_for('login'))
-    #     print(url_for('login', next='/'))
-    #     print(url_for('profile', username='John Doe'))
+        return render_template('base.html', title='DB Reset', users=[])
     return app
